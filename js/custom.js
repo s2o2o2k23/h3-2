8aa85821b27668dd61cf115f9b88f6a0289a9b5e
s2o2o2k23 <s2o2o2k23@naver.com>
2024-08-21 17:03:18 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+    })
+});
+
+
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+        //dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        //useTransform: false
+    });
+})
